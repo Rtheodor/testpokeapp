8b1aa6a9016c6baa75d8d67706986018f013dc88
Rafael Theodoro <rafaeldacunhatheodoro@gmail.com>
2025-06-17 15:01:23 -0300

    Mensagem do commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'testpokeapp',
+  webDir: 'www'
+};
+
+export default config;
